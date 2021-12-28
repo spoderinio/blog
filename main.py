eb85e8c3d3e63e78348965fae36f0dffcf38d708
Plamen Atanasov <spoder@gmail.com>
2021-12-28 17:20:38 +0200

    completed project

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash, abort
+from flask import Flask, render_template, redirect, url_for, flash, abort, request
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
@@ -18,6 +18,15 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
+gravatar = Gravatar(app,
+                    size=100,
+                    rating='g',
+                    default='retro',
+                    force_default=False,
+                    force_lower=False,
+                    use_ssl=False,
+                    base_url=None)
+
 login_manager.init_app(app)
 
 # CONNECT TO DB
@@ -32,18 +41,16 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
-
+    name = db.Column(db.String(100))
     posts = relationship("BlogPost", back_populates="author")
-    comments = relationship("Comment", back_populates="author")
+    comments = relationship("Comment", back_populates="comment_author")
 
 
 class BlogPost(db.Model):
     __tablename__ = "blog_posts"
     id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer, ForeignKey('users.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     author = relationship("User", back_populates="posts")
-    # author = db.Column(db.String(250), nullable=False)
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
@@ -55,13 +62,11 @@ class BlogPost(db.Model):
 class Comment(db.Model):
     __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
     author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    comment_author = relationship("User", back_populates="comments")
-    post_id = db.Column(db.Integer, ForeignKey("blog_posts.id"))
     parent_post = relationship("BlogPost", back_populates="comments")
+    comment_author = relationship("User", back_populates="comments")
     text = db.Column(db.Text, nullable=False)
-
-# db.create_all()
 
 
 db.create_all()
@@ -150,17 +155,28 @@ def logout():
     return redirect(url_for('get_all_posts'))
 
 
-@app.route("/post/<int:post_id>")
+@app.route("/post/<int:post_id>", methods=["GET", "POST"])
 def show_post(post_id):
     form = CommentForm()
     requested_post = BlogPost.query.get(post_id)
     if current_user.is_authenticated:
+        if request.method == "POST":
+            new_comment = Comment(
+                text=form.comment.data,
+                comment_author=current_user,
+                parent_post=requested_post
+            )
+            db.session.add(new_comment)
+            db.session.commit()
         user_id = current_user.id
         # return f"User is {user_id}"
         if user_id == 1:
             return render_template("post.html", form=form, post=requested_post, admin=user_id, logged_in=current_user.is_authenticated)
-
-    return render_template("post.html", post=requested_post, logged_in=current_user.is_authenticated)
+    else:
+        if request.method == "POST":
+            flash("You need to login to comment!")
+            return redirect(url_for('login'))
+    return render_template("post.html", form=form, post=requested_post, logged_in=current_user.is_authenticated)
 
 
 @app.route("/about")
@@ -207,7 +223,6 @@ def edit_post(post_id):
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
         post.img_url = edit_form.img_url.data
-        post.author = edit_form.author.data
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id), logged_in=current_user.is_authenticated)
