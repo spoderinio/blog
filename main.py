c2eb5396a9014a5a470b865c26eda195d38ac660
Plamen Atanasov <spoder@gmail.com>
2021-12-29 15:10:39 +0200

    postgreSQL

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from sqlalchemy import Table, Column, Integer, ForeignKey
 import os
 
 login_manager = LoginManager()
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 ckeditor = CKEditor(app)
@@ -31,7 +31,7 @@ gravatar = Gravatar(app,
 login_manager.init_app(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
