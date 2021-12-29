8e4cbfa93c352927a5ba041e8ab01386fc7f64cf
Plamen Atanasov <spoder@gmail.com>
2021-12-29 10:36:33 +0200

    add env variable

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 from sqlalchemy import Table, Column, Integer, ForeignKey
+import os
 
 login_manager = LoginManager()
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY']
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
